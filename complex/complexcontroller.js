335b41bf8cb2795ff6014c07200258650fe264e7
Dave Kerr <dave_kerr@mckinsey.com>
2015-01-13 15:54:22 +0530

    Added the programmatic closure sample.

--- complex/complexcontroller.js
+++ complex/complexcontroller.js
@@ -11,7 +11,21 @@ app.controller('ComplexController', ['$scope', 'title', 'close', function($scope
       name: $scope.name,
       age: $scope.age
     }, 500); // close, but give 500ms for bootstrap to animate
- 
- };
+  };
+
+  //  This cancel function must use the bootstrap, 'modal' function because
+  //  the doesn't have the 'data-dismiss' attribute.
+  $scope.cancel = function() {
+
+    //  Manually hide the modal.
+    $element.modal('hide');
+    
+    //  Now call close, returning control to the caller.
+    close({
+      name: $scope.name,
+      age: $scope.age
+    }, 500); // close, but give 500ms for bootstrap to animate
+  };
+
 
 }]);

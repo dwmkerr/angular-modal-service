be42b165e78557ddf40cd3b278c18fb38bf650ea
Dave Kerr <dwmkerr@gmail.com>
2016-06-15 10:20:45 +0800

    Unit tests now use angular as installed by npm, rather than bower, simplifying the process and resolving issues with bower not being installed.

--- karma.config.js
+++ karma.config.js
@@ -12,8 +12,8 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       //  Dependencies
-      'bower_components/angular/angular.min.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      './node_modules/angular/angular.js',
+      './node_modules/angular-mocks/angular-mocks.js',
 
       'src/angular-modal-service.js',
 

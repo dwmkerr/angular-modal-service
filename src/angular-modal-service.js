632127854adc2f96396cc780b494de99853ea75e
Serg <sergeyc@gmail.com>
2016-12-09 13:25:41 -0500

    On Location Change don't pass event as result
    I think when closing the modal on location change it shouldn't pass event as the result .

--- src/angular-modal-service.js
+++ src/angular-modal-service.js
@@ -61,7 +61,9 @@ module.factory('ModalService', ['$animate', '$document', '$compile', '$controlle
 
           //  Create a new scope for the modal.
           var modalScope = (options.scope || $rootScope).$new();
-          var rootScopeOnClose = $rootScope.$on('$locationChangeSuccess', cleanUpClose);
+          var rootScopeOnClose = $rootScope.$on('$locationChangeSuccess', function(event){
+            cleanUpClose();
+          });
 
           //  Create the inputs object to the controller - this will include
           //  the scope, as well as all inputs provided.

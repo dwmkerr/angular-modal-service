a907b0e2ebdb9492e04deb53c5872d5069e2d736
George DeCherney <gdecherney@churnzero.net>
2018-11-09 10:35:31 -0500

    updated the complex example to show how to handle bootstraps background click dismisal

--- samples/complex/complexcontroller.js
+++ samples/complex/complexcontroller.js
@@ -1,34 +1,73 @@
 var app = angular.module('sampleapp');
 
 app.controller('ComplexController', [
-  '$scope', '$element', 'title', 'close', 
-  function($scope, $element, title, close) {
+    '$scope', '$element', 'title', 'close',
+    function ($scope, $element, title, close) {
 
-  $scope.name = null;
-  $scope.age = null;
-  $scope.title = title;
-  
-  //  This close function doesn't need to use jQuery or bootstrap, because
-  //  the button has the 'data-dismiss' attribute.
-  $scope.close = function() {
- 	  close({
-      name: $scope.name,
-      age: $scope.age
-    }, 500); // close, but give 500ms for bootstrap to animate
-  };
+        $scope.name = null;
+        $scope.age = null;
+        $scope.title = title;
 
-  //  This cancel function must use the bootstrap, 'modal' function because
-  //  the doesn't have the 'data-dismiss' attribute.
-  $scope.cancel = function() {
+        $scope.wasClosed = false;
 
-    //  Manually hide the modal.
-    $element.modal('hide');
-    
-    //  Now call close, returning control to the caller.
-    close({
-      name: $scope.name,
-      age: $scope.age
-    }, 500); // close, but give 500ms for bootstrap to animate
-  };
+        //  This close function doesn't need to use jQuery or bootstrap, because
+        //  the button has the 'data-dismiss' attribute.
+        $scope.close = function () {
 
-}]);
+            //mark the modal as handled
+            $scope.wasClosed = true;
+
+            close({
+                name: $scope.name,
+                age: $scope.age,
+                source: 'Ok\'ed'
+            }, 500); // close, but give 500ms for bootstrap to animate
+        };
+
+        //  This cancel function must use the bootstrap, 'modal' function because
+        //  the doesn't have the 'data-dismiss' attribute.
+        $scope.cancel = function () {
+
+            //mark the modal as handled
+            $scope.wasClosed = true;
+
+            //  Manually hide the modal.
+            $element.modal('hide');
+
+            //  Now call close, returning control to the caller.
+            close({
+                name: $scope.name,
+                age: $scope.age,
+                source: 'Cancelled'
+            }, 500); // close, but give 500ms for bootstrap to animate
+        };
+
+        //  The abort function  doesn't need to use jQuery or bootstrap, because
+        //  the backdrop click already dismisses the modal and the X has the 'data-dismiss' attribute.
+        $scope.abort = function () {
+            //mark the modal as handled
+            $scope.wasClosed = true;
+
+            close({
+                name: $scope.name,
+                age: $scope.age,
+                source: 'Aborted'
+            }, 500); // close, but give 500ms for bootstrap to animate
+        };
+
+        var backgroundClickHandler = function (e) {
+
+            //if the modal was already closed short circuit
+            if ($scope.wasClosed) return;
+
+            //call the close functionality
+            $scope.abort();
+
+            //remove the listener
+            $element.off('hidden.bs.modal', backgroundClickHandler);
+        };
+
+        //listen for when the modal is dismissed and close it
+        $element.on('hidden.bs.modal', backgroundClickHandler);
+
+    }]);
